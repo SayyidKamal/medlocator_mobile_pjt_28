ce5f0c0947e1d3e7b520a514e1a1ccdeb51445c2
Ab3id <abeidtwo@gmail.com>
2021-06-27 11:36:28 +0300

    feat: add WelcomeScreen.kt

--- app/src/main/java/team28/zuri/medlocator/Activities/SplashActivity.kt
+++ app/src/main/java/team28/zuri/medlocator/Activities/SplashActivity.kt
@@ -12,7 +12,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this,WelcomeScreen::class.java))
             finish()
         }, SPLASH_TIME_OUT)
     }
